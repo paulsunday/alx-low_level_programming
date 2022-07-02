f475d6a230b43de9c8a8abe38af664132a9c4374
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-02 12:43:49 -0700

    wrote a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main- Entry point for the program
+*Return: always zero "0" (Success)
+*/
+int main(void)
+{
+int n = 0;
+for (n = 0; n <= 10; n++)
+printf("%i\n", n);
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -4,3 +4,4 @@
 2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
 alphABET
 When I was having that alphabet soup, I never thought that it would pay off
+5. Numbers
