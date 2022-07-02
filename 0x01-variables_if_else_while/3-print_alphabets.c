3e8a838aea54962e9ab320793a193a2d9133574a
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-02 04:08:19 -0700

    This is a program that prints uppercase and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main- Entry point for the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
