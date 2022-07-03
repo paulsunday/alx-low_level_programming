6587e4f697cb341de173edfc54a2726756b2f23b
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-02 18:52:03 -0700

    quick fix v2.1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 int a, b, c;
-for (a = 0; a <= 9; a++)
+for (a = 0; a < 8; a++)
 {
-for (b = a++; b <= 9; b++)
+for (b = a++; b < 9; b++)
 {
-for (c = b++; c < 9; c++)
+for (c = b++; c < 10; c++)
 {
 putchar(a + '0');
 putchar(b + '0');
 putchar(c + '0');
-if (a == 9 && b == 9 && c == 9)
+if (a == 7 && b == 8 && c == 9)
 continue;
 putchar(',');
 putchar(' ');
