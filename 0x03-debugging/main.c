928fc2f70ed2c5a2560f2ddcf4ef471c933c9185
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-07 06:57:25 -0700

    task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main- tests the function that prints if integers is positive or negative
+* Return : 0
+*/
+int main(void)
+{
+int j;
+j = 0;
+positive_or_negative(j);
+return (0);
+}
