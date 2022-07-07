c7d9e3f04eafcac67006e43bd2615bef0d0e313a
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-07 09:17:35 -0700

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,12 @@
-positive_or_negative();
+#include "main.h"
+/**
+* main- test function that prints if interger is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,1 @@
+int postitive_or_negative();
