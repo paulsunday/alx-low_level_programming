037bb1fc1ad71458f657ac3f1f57e4ffbc0a17a2
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-15 08:48:07 -0700

    task: 11

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 for (i = 0; i < n && *(src + i); i++)
 *(dest + i) = *(src + i);
-for ( ; i < n; i++)
-*dest[i] = '\0';
+for (; i < n; i++)
+*(dest + i) = '\0';
 return (dest);
 }
