55e4944fbabf9b3394cc864ac5cf7eb419494bdc
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-15 14:31:33 -0700

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -17,7 +17,7 @@ for (j = 0; n2[j]; j++)
 if (i > size_r || j > size_r)
 return (0);
 m = 0;
-for (i -= 1, j =- 1, k = 0; k < size_r - 1; i--, j--, k++)
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
 n = m;
 if (i >= 0)
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,6 +4,7 @@
 * @dest: detion string
 * @src: pointer
 * @n: int
+* Return: returns pointer to string
 */
 char *_strncpy(char *dest, char *src, int n)
 {
