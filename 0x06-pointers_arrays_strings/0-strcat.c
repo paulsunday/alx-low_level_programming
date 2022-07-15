6ec33362d4cc69f18f0559500d5fbaacd584fc5e
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-15 03:20:41 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,17 @@
 */
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++)
-;
-do {
-a++;
-dest[i] = src[a];
-i++;
-} while (src[a] != '\0');
-return (dest);
+char *s = dest;
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (s);
 }
