92cfd4334771f8232ed2d3f71c3113a988861c71
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-18 19:18:39 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _strstr-Finds the first occurance of substring
+* @haystack: String
+* @needle: substring in string
+* Return: null
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int a;
+int len = 0;
+int b;
+for (a = 0; haystack[a] != '\0'; a++)
+{
+len++;
+}
+for (a = 0; haystack[a] != '\0'; a++)
+{
+if (haystack[a] == 32) /* 32 is space in ascii*/
+{
+haystack[a]++;
+for (b = 0; needle[b] != '\0'; b++)
+{
+if (haystack[a] == needle[b])
+return (needle);
+}
+}
+}
+return ('\0');
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ Task 1: Write a function that copies memory area.
 Task 2: Write a function that locates a character in a string.
 Task 3: Write a function that gets the length of a prefix substring.
 Task 4: Write a function that searches a string for any of a set of bytes.
+Task 5:Write a function that locates a substring.
