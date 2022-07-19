04fc8a2abc8a674e3b365fe616107f752106eee7
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-18 17:11:03 -0700

    Task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,11 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+unsigned int i = 0;
 if (i < n)
 {
 s[i] = b;
-i++;
+ i++;
 }
 return (s);
 }
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
 Task 0: Write a function that fills memory with a constant byte.
+Task 1: Write a function that copies memory area.
