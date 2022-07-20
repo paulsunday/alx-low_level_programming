e8390a22000da8711ac9df184539d46c97951744
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-20 16:07:03 -0700

    Task 4:

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _pow_recursion- Return the value of x raise to the power of 1
+* @x: first num
+* @y: Second Num
+* Return: Always zero
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (0);
+}
+return (x * _pow_recursion(x, y - 1));
+}
