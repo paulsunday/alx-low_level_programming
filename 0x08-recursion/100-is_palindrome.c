095996d5cf597b361ee940dece7694034952982a
paulsunday <paulsundayigwegbe@gmail.com>
2022-07-21 18:10:43 -0700

    fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,39 @@
 #include "main.h"
 /**
-* is_palindrome- AString that is a palindrome
+* _strlen- AString that is a palindrome
 * Return: Empty String
 * @s: String
 */
+int _strlen(char *s)
+{
+if (s[0] != '\0')
+return (1 + _strlen(s + 1));
+return (0);
+}
+/**
+* pal_checker- check if s is palindrome.
+* @s: string base address
+* @i: left index
+* @j: right index
+* Return: 1 if s is a palindrome,
+*/
+int pal_checker(char *s, int i, int j)
+{
+if (s[i] == s[j])
+if (i > j / 2)
+return (1);
+else
+return (pal_checker(s, i + 1, j - 1));
+else
+return (0);
+}
+/**
+* is_palindrome- Checks if s is palindrome
+* @s: base address for string
+*
+* Return: 1 if n is prime
+*/
 int is_palindrome(char *s)
 {
-char *p;
-is_palindrome(s + 1);
-p = &(*s);
-if (*p == *s)
-{
-return (1);
-}
-return (0);
+return (pal_checker(s, 0, _strlen(s) - 1));
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,20 +1,31 @@
 #include "main.h"
 /**
-* wildcmp- Compares two strings
-* @s1: First string to be comared
-* @s2: Second String to be compared
-*
-* Return: 1 if identical else 0
+* str_checker - check if two strings are identical.
+* @s1: string_1 base address.
+* @s2: string_2 base address.
+* @i: left index.
+* @j: special index. (joker)
+* Return: 1 if s is palindrome, 0 otherwise.
+*/
+int str_checker(char *s1, char *s2, int i, int j)
+{
+if (s1[i] == '\0' && s2[j] == '\0')
+return (1);
+if (s1[i] == s2[j])
+return (str_checker(s1, s2, i + 1, j + 1));
+if (s1[i] == '\0' && s2[j] == '*')
+return (str_checker(s1, s2, i, j + 1));
+if (s2[j] == '*')
+return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+return (0);
+}
+/**
+* wildcmp - check if strings could be considered identical
+* @s1: base address for string.
+* @s2: base address for string.
+* Return: 1 if are considered identical.
 */
 int wildcmp(char *s1, char *s2)
 {
-if (!*s1 && !*s2)
-return (1);
-if (*s1 == *s2)
-return (wildcmp(s1 + 1, s2 + 1));
-if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-return (1);
-if (*s2 == '*' && *(s1 + 1) && *s2)
-return (0);
-return (0);
+return (str_checker(s1, s2, 0, 0));
 }
