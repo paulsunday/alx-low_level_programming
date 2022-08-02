7af8b6598129d91b57f302bd3759d795cc5c57a0
paulsunday <paulsundayigwegbe@gmail.com>
2022-08-02 13:24:01 -0700

    TASK 1

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+TASK 1:
+
+Define a new type struct dog with the following elements:
+name, type = char *
+age, type = float
+owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG
+#define DOG
+/**
+* struct dog - dog class
+* @name: The dogs name
+* @age: the dog's age
+* @owner: the dog's owner
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
