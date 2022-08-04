c3b17c8f6611bfd546f1827c5efd274de6274450
paulsunday <paulsundayigwegbe@gmail.com>
2022-08-03 21:05:28 -0700

    TASK 3

---
+++ 0x0F-function_pointers/3-calc.h~
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+#endif
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+* get_op_func- gets the correct function to perform the operation
+* @s: operator passed
+* Return: pointer to the function that corresponds to operator given
+*/
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (i < 5)
+{
+if (*(ops[i]). op == *s)
+return (ops[i].func);
+i++;
+}
+return (NULL);
+}
