582746a4d648c5f0a890cdf1c2268bc93d0af8f5
paulsunday <paulsundayigwegbe@gmail.com>
2022-08-04 18:49:33 -0700

    TASK 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* c - prints character
+*/
+char c(char a)
+{
+if (a)
+printf("%c", a);
+}
+/**
+* print_all - prints anything
+*
+* @format: format of the string to be printed
+*/
+void print_all(const char * const format, ...)
+{
+va_list
+}
