7fa8f9f497280c7b8d4e4614ed3f060ceaf8d40a
paulsunday <paulsundayigwegbe@gmail.com>
2022-08-11 21:55:03 -0700

    fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -33,14 +33,14 @@ else
 return (new);
 }
 /**
- * _strlen_recursion- returns the length of a string
- * @s: string
- * Return: length of @s
- */
+* _strlen_recursion- returns the length of a string
+* @s: string
+* Return: length of @s
+*/
 int _strlen_recursion(char *s)
 {
-  if (*s == 0)
-    return (0);
-      else
-	return (1 + _strlen_recursion(s + 1));
-      }
+if (*s == 0)
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
+}
