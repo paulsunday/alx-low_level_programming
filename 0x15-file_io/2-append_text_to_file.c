40aed4546b1d56dabc656bcb43f8e3b5600f366e
Igwegbe Sunday Paul <78316709+paulsunday@users.noreply.github.com>
2022-08-24 05:27:34 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,26 +1,32 @@
 #include "main.h"
+
 /**
- * _strlen - finds the len of a string
+ * _strlen - finds the length of a string
  * @str: pointer to the string
- * Return: len of the string
+ *
+ * Return: length of the string
  */
 size_t _strlen(char *str)
 {
 	size_t i;
+
 	for (i = 0; str[i]; i++)
 		;
 	return (i);
 }
+
 /**
- * append_text_to_file- appends a text at the end of a file.
+ * append_text_to_file - appends a text at the end of a file.
  * @filename: name of the file
- * @text_content: NULL terminated string to add at the endof the file 
- * Return: 1 on success and -1 on fialure
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t len;
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
