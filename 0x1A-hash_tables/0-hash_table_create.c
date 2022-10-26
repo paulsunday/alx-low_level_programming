377d7bcf9e6e1a311780ff32d0c2e7d7ec3025eb
paulsunday <paulsundayigwegbe@gmail.com>
2022-10-25 17:00:26 -0700

    TASK 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -17,7 +17,7 @@ typedef struct hash_node_s
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-}
+} hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
