b94990fbbc3fe74782fcfb47c8570352f3a3aa46
Igwegbe Sunday Paul <78316709+paulsunday@users.noreply.github.com>
2022-10-26 01:01:53 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,7 @@
-#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_create- creates a hash table
+ * hash_table_create - creates a hash table
  * @size: size of the array
  *
  * Return: pointer to the newly created hash table
